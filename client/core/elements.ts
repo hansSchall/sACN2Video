dcac18fafdff4562927d98766548c49668725c87
Hans Schallmoser <hans-schallmoser@gmx.de>
2022-08-08 14:27:19 +0200

    move elements to their own files

--- client/core/elements.ts
+++ client/core/elements.ts
@@ -46,47 +46,117 @@ async function loadElmnts() {
                 if (rootLock) {
                     console.error(`found more than one root config`);
                 } else {
-                    const updateParImpl = {
-                        updatePars(par: string, value: string | number, sacn?: boolean): void {
-                            const corner = par.slice(0, 2);
-                            const coord = par.slice(2);
-                            if (/^(T|B)(L|R)(X|Y)$/.test(par) && corner.length == 2 && coord.length == 1) {
-                                transformProps
-                                [tranformCorner(corner) ?? 0]
-                                [coord == "X" ? 0 : 1] =
-                                    parseFloat(value as string);
-                            } else if (par == "mask") {
-                                textureLoadIndicator(false);
-                                const img = new Image();
-                                img.src = assets.get(value.toString()) ?? "";
-                                useMask = true;
-                                img.addEventListener("load", () => {
-                                    if (!gl) {
-                                        throw new Error("WebGLContext is undefined");
-                                    }
-                                    gl.bindTexture(gl.TEXTURE_2D, lg.maskTex);
-                                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-                                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                                    console.log(`%c [${timeSinceAppStart()}] mounted mask`, "color: #0f0");
-                                    textureLoadIndicator(true);
-                                })
-                            } else if (par == "fbH") {
-                                setFBsize({
-                                    h: int(value)
-                                })
-                            } else if (par == "fbW") {
-                                setFBsize({
-                                    w: int(value)
-                                })
-                            }
-                        }
-                    }
-                    props.forEach(Elmnt.prototype.initPar.bind(updateParImpl));
+                    rootElement(props)
                 }
                 break;
         }
     }
     clear();
+}
+
+
+type Prop = [string, string, string];
+
+const elmnts = new Set<Elmnt>();
+abstract class Elmnt {
+    constructor(readonly id: string) {
+        if (!gl) {
+            throw new Error("WebGLContext is undefined");
+        }
+        this.tex = undefinedMsg(gl.createTexture(), "texture creation failed");
+        gl.bindTexture(gl.TEXTURE_2D, this.tex);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    }
+    protected tex: WebGLTexture;
+    getOpacity() {
+        return this.opacity;
+    }
+    protected opacity: number = 0;
+    bindTex(bindPoint: number = gl?.TEXTURE_2D || 0) {
+        gl?.bindTexture(bindPoint, this.tex);
+    }
+    bufferPos() {
+        gl?.bufferData(gl.ARRAY_BUFFER, Pos2Buffer(this.pos), gl.DYNAMIC_DRAW);
+    }
+    pos: Pos = { x: 0, y: 0, h: 1, w: 1 };
+    updatePars(par: string, value: string | number, sacn?: boolean): void {
+        switch (par) {
+            case "intens":
+            case "i":
+            case "o":
+            case "intensity":
+                this.opacity = parseFloat(value as string);
+                break;
+            case "x":
+                this.pos.x = parseFloat(value as string);
+                break;
+            case "y":
+                this.pos.y = parseFloat(value as string);
+                break;
+            case "h":
+                this.pos.h = parseFloat(value as string);
+                break;
+            case "w":
+                this.pos.w = parseFloat(value as string);
+                break;
+        }
+    }
+    initPar([name, type, value]: Prop) {
+        type = type.toLowerCase();
+        function addSacnListener(addr: number, listener: (this: Elmnt, value: number) => void) {
+            if (sacnListener.has(addr)) {
+                sacnListener.get(addr)?.add(listener);
+            } else {
+                sacnListener.set(addr, new Set([listener]));
+            }
+        }
+
+        if (type.startsWith("static")) {
+            if (type == "staticcp") {
+                this.updatePars(name, (parseFloat(value) + 1) / 2, false);
+            } else if (type == "staticpcp") {
+                this.updatePars(name, (parseFloat(value) + 100) / 200, false);
+            } else {
+                this.updatePars(name, value, false);
+            }
+        } else if (type == "sacn") {
+            const rawaddr = value.split(/(\/|\+|\,|\.|\\)/).map(_ => parseInt(_)).filter(_ => !isNaN(_));
+            let addr: number | [number, number]
+            if (rawaddr.length == 1) { // addr
+                addr = rawaddr[0];
+            } else if (rawaddr.length == 2) { // universe/addr
+                addr = (rawaddr[0] - 1) * 512 + rawaddr[1];
+            } else if (rawaddr.length == 3) { // universe/low/high
+                addr = [((rawaddr[0] - 1) * 512 + rawaddr[1]), ((rawaddr[0] - 1) * 512 + rawaddr[2])];
+            } else if (rawaddr.length == 4) { // universelow/low/universehigh/high
+                addr = [((rawaddr[0] - 1) * 512 + rawaddr[1]), ((rawaddr[2] - 1) * 512 + rawaddr[3])];
+            } else {
+                throw new Error("sacn value.length is not in the range of 1-4");
+            }
+
+            if (typeof addr == "number") { // 8
+                addSacnListener(addr, value => {
+                    this.updatePars(name, value / 255, true);
+                });
+            } else { // 16
+                let valuelow = 0, valuehi = 0;
+                const updateValue = (() => {
+                    this.updatePars(name, ((valuehi << 8) + valuelow) / 65535, true);
+                }).bind(this);
+                addSacnListener(addr[0], value => {
+                    valuelow = value;
+                    updateValue();
+                });
+                addSacnListener(addr[1], value => {
+                    valuehi = value;
+                    updateValue();
+                });
+            }
+        } else {
+            console.warn(`type of ${this.id}.${name} is unknown`);
+        }
+    }
 }
---
+++ client/core/elements/image.ts
@@ -0,0 +1,29 @@
+class ImgElmnt extends Elmnt {
+    constructor(id: string, props: Prop[]) {
+        super(id);
+        if (!gl) {
+            throw new Error("WebGLContext is undefined");
+        }
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1]), gl.STATIC_DRAW);
+        const img = new Image();
+        img.src = assets.get(props.find(_ => _[0] == "src")?.[2] ?? "") ?? "";
+        textureLoadIndicator(false);
+        img.addEventListener('load', () => {
+            if (!gl) {
+                throw new Error("WebGLContext is undefined");
+            }
+            // Now that the image has loaded make copy it to the texture.
+            gl.bindTexture(gl.TEXTURE_2D, this.tex);
+            gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+            console.log(`%c [${timeSinceAppStart()}] mounted ${id}`, "color: #0f0");
+            textureLoadIndicator(true);
+        });
+        props.forEach(this.initPar.bind(this));
+    }
+
+
+}
