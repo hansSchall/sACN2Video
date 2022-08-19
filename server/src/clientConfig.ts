a51e930b2ff7c339680be9a5251574b24fa46025
Hans Schallmoser <99032404+hansSchall@users.noreply.github.com>
2022-08-19 16:31:27 +0200

    add clientConfigV2

--- server/src/clientConfig.ts
+++ server/src/clientConfig.ts
@@ -1,4 +1,5 @@
 import { db } from "./db.js";
+import { joincomma } from "./utils";
 
 export async function clientConfig(): Promise<string> {
     return JSON.stringify(
@@ -9,4 +10,26 @@ export async function clientConfig(): Promise<string> {
                 (await db.all("SELECT prop, valueType, value FROM elProps WHERE el = ?", _.id))
                     .map(_ => ([_.prop, _.valueType, _.value]))
             ])))
+}
+
+export async function clientConfigV2(): Promise<string> {
+    return joincomma(await Promise.all((await db.all("SELECT id, type FROM els WHERE enabled = 1 ORDER BY zi"))
+        .map(async elItem => joincomma([
+            elItem.id,
+            elItem.type,
+            ...(await Promise.all(
+                (await db.all("SELECT prop, valueType, value FROM elProps WHERE el = ?", elItem.id))
+                    .map(async ({ prop, valueType, value }) => {
+                        const { input, output } = await db.get("SELECT input, output FROM propMapping WHERE el = ? AND prop = ?") || {
+                            input: "",
+                            output: ""
+                        }
+                        return joincomma([
+                            prop, valueType, value, input, output
+                        ]);
+                    })
+            ))
+        ])
+        )
+    ))
 }
